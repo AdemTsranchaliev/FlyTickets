e47719daeaf81952d0d75464549c926b739ea936
adem tsranchaliev <ademcran4aliev@abv.bg>
2022-04-26 19:22:52 +0300

    Init

---
+++ FlyTickets/Data/Reservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyTickets.Data
+{
+    public class Reservation
+    {
+        public Guid Id { get; set; }
+        public int RouteId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Laguage { get; set; }
+    }
+}
